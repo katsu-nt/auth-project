2be9d767f6666d9048fd490737392946c6b98744
ben <benbilldev@gmail.com>
2024-09-08 12:24:27 +0700

    Auth - Logout&Login: api endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
-import { sendVerificationEmail } from "../mailtrap/emails.js";
+import { sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/emails.js";
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
   try {
@@ -29,20 +29,86 @@ export const signup = async (req, res) => {
     await user.save();
     //jwt
     generateTokenAndSetCookie(res, user._id);
-    await sendVerificationEmail(user.email,user.verificationToken);
-    res
-      .status(201)
-      .json({ success: true, message: "User created successfully", user:{
+    await sendVerificationEmail(user.email, user.verificationToken);
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
         ...user._doc,
-        password: undefined
-      } });
+        password: undefined,
+      },
+    });
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
   }
 };
+export const verifyEmail = async (req, res) => {
+  const { code } = req.body;
+  try {
+    const user = await User.findOne({
+      verificationToken: code,
+      verificationExpiresAt: { $gt: Date.now() },
+    });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid or exprired verification code",
+      });
+    }
+    user.isVerified = true;
+    user.verificationToken = undefined;
+    user.verificationExpiresAt = undefined;
+    await user.save();
+    await sendWelcomeEmail(user.email, user.name);
+    res.status(200).json({
+      success: true,
+      message: "Email verified successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
 export const login = async (req, res) => {
- 
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({
+      email,
+    });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+    generateTokenAndSetCookie(res, user._id);
+    user.lastLogin = new Date();
+    await user.save();
+    res.status(200).json({
+      success: true,
+      message: "Logged in successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 export const logout = async (req, res) => {
-  
+  res.clearCookie("token");
+  res.status(200).json({ success: true, message: "Logged out successfully" });
 };
--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,18 +1,38 @@
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplate.js"
-import { mailtrapClient, sender } from "./mailtrap.config.js"
+import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplate.js";
+import { mailtrapClient, sender } from "./mailtrap.config.js";
 
-export const sendVerificationEmail = async (email,verificationToken)=>{
-    const recipient = [{email}]
-    try {
-        const response = await mailtrapClient.send({
-            from:sender,
-            to:recipient,
-            subject:"Verify your email",
-            html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}",verificationToken),
-            category:"Email Verification"
-        })
-        console.log('Email send successfully ',response)
-    } catch (error) {
-        throw new Error(`Error sending verification email: ${error}`)
-    }
-}
+export const sendVerificationEmail = async (email, verificationToken) => {
+  const recipient = [{ email }];
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Verify your email",
+      html: VERIFICATION_EMAIL_TEMPLATE.replace(
+        "{verificationCode}",
+        verificationToken
+      ),
+      category: "Email Verification",
+    });
+    console.log("Email send successfully ", response);
+  } catch (error) {
+    throw new Error(`Error sending verification email: ${error}`);
+  }
+};
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }];
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "29889afd-8dbb-44ae-9d9e-f0fa75a12d71",
+      template_variables: {
+        company_info_name: "Le Hoang Company",
+        name: name,
+      },
+    });
+    console.log("Welcome Email send successfully ", response);
+  } catch (error) {
+    throw new Error(`Error sending verification email: ${error}`);
+  }
+};
